72eac5fadf896f2d375cb259343019e37d07fdb1
Antoine Pelisse <apelisse@gmail.com>
2011-01-28 15:33:28 +0100

    Initial Commit.
    Let's add two pretty empty files:
    - dot.emacs.el is the file loaded by emacs which should call our git
    repository
    - emacs-config.el is the entry point for all our options. Once we are
    in the git repository. This file can call all other files.

---
+++ dot.emacs.el
@@ -0,0 +1,10 @@
+;; -*- mode: emacs-lisp -*-
+;;
+;; Emacs config file (dot.emacs.el)
+;; This file is to be loaded automatically by emacs and should then be
+;; renamed `~/.emacs.el'.
+
+;; It does nothing but load all files from the git repository (which
+;; should be named `~/.emacs.d'
+
+(load-file "~/.emacs.d/emacs-config.el")
---
+++ emacs-config.el
@@ -0,0 +1,6 @@
+;; -*- mode: emacs-lisp -*-
+
+;; Entry config file (`emacs-config.el')
+;; The is the entry point for emacs configuration.
+
+;; Nothing yet
