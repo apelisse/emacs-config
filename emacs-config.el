185a2d553fb27394b2e5320eb6daa0fa75d9454b
Antoine Pelisse <apelisse@gmail.com>
2011-01-28 18:58:03 +0100

    Very simple configuration
    Simply adds a dfault brower and start the server so we can connect from
    console to commit or everything.

--- emacs-config.el
+++ emacs-config.el
@@ -3,4 +3,9 @@
 ;; Entry config file (`emacs-config.el')
 ;; The is the entry point for emacs configuration.
 
-;; Nothing yet
+;; Let's start emacs-server, pretty useful when committing files from VCS
+(server-start)
+
+;; Configure web brower
+(setq browse-url-browser-function (quote browse-url-generic))
+(setq browse-url-generic-program "firefox3")
