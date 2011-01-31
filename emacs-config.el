db7176a43acaa54c8bdea1cb8f181baa9be81b8d
Antoine Pelisse <antoine.pelisse@amadeus.com>
2011-01-31 09:00:28 +0100

    Add most options to configuration

--- emacs-config.el
+++ emacs-config.el
@@ -1,4 +1,4 @@
-;; -*- mode: emacs-lisp; coding: utf-8-emacs -*-
+;; emacs-config.el -*- mode: emacs-lisp; coding: utf-8-emacs -*-
 
 ;; Entry config file (`emacs-config.el')
 ;; The is the entry point for emacs configuration.
@@ -12,3 +12,58 @@
 
 ;; Allow disabled functions
 (put 'narrow-to-region 'disabled nil) ;; narrow/widen region
+(put 'upcase-region 'disabled nil) ;; upcase region
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Display Configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq inhibit-startup-screen t)
+(setq show-trailing-whitespace t)
+(setq inhibit-splash-screen t)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+(setq tab-width 4)
+(setq default-tab-width 4)
+(show-paren-mode 1)
+(menu-bar-mode -1)
+(iswitchb-mode t)
+(global-hi-lock-mode 1)
+(column-number-mode 1)
+(setq normal-erase-is-backspace 0)
+(setq scroll-bar-mode (quote right))
+
+;; Colors for compilation buffer
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq org-log-done t)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Backups
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq version-control t)
+(setq delete-old-versions t)
+(add-to-list 'backup-directory-alist (cons "." "~/.emacs.d/backups/"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emerge
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'emerge-startup-hook '(emerge-skip-prefers 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Eshell
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'eshell-mode-hook
+    '(lambda ()
+	   (define-key eshell-mode-map [up] 'previous-line)
+	   (define-key eshell-mode-map [down] 'next-line)))
