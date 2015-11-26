976ed4de6b0e4cc17b83bd69f6073a2667272d3b
Antoine Pelisse <apelisse@gmail.com>
2015-11-25 16:30:41 -0800

    Get C++ style closer to Tango style

--- tango.el
+++ tango.el
@@ -1,15 +1,17 @@
 (defconst tango-style
   '("gnu"
-    (c-tab-always-indent        . nil)
     (c-offsets-alist . ((innamespace . [0])
-			(substatement-open . 0)
-			(arglist-intro . +)))))
+			(arglist-intro . +)
+			(arglist-close . 0)
+			(statement-cont . 0)
+			(member-init-intro . ++)))))
 
 (c-add-style "tango-style" tango-style)
 
 (add-hook 'c++-mode-hook
   '(lambda ()
-     (message "loading c++ mode hook")
+     (setq indent-tabs-mode nil)
+     (whitespace-mode 0)
      (c-set-style "tango-style")))
 
 (add-hook 'python-mode-hook
