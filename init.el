9ffd27185664b1b9564b4338bbcc371f95398a78
Antoine Pelisse <apelisse@gmail.com>
2019-05-09 20:55:32 -0700

    Always run gofmt before save

--- init.el
+++ init.el
@@ -268,3 +268,7 @@
 ;; go-mode
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+(add-hook 'go-mode-hook
+	  '(lambda ()
+	     (add-hook 'before-save-hook
+		       'gofmt-before-save)
