0b2e083baadea2918c4b93b5bcb22bed740f3cd8
Antoine Pelisse <apelisse@gmail.com>
2022-12-04 20:13:27 -0800

    Use use-package to configure lsp-mode with rust-analyzer

--- init.el
+++ init.el
@@ -237,6 +237,26 @@
 	    (setq indent-tabs-mode nil)))
 (add-hook 'rust-mode-hook #'lsp-deferred)
 
+(use-package lsp-mode
+  :ensure
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints t)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+
+  :custom-face
+  (lsp-rust-analyzer-inlay-face ((t (:foreground "gray33" :slant italic :height 110)))))
+
 ;; YAML mode
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
